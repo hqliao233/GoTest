4f6d2bb065d65062ca116ef7e1fc953ed3a7ef68
LiaoHQ <1915443708@qq.com>
2020-12-14 22:23:17 +0800

    新增url判断

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func handerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 这里是goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "这里是关于blog的内容")
+	} else {
+		fmt.Fprint(w, "_我不知道你在说什么_")
+	}
+
 }
 
 func main() {
