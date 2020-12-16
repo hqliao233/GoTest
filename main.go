67be2bfbb96d297f5b5dd97b8e5175461fda2ffb
LiaoHQ <1915443708@qq.com>
2020-12-16 22:05:22 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func handerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是goblog</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "这里是关于blog的内容")
+		fmt.Fprint(w, "这里是关于blog的内容12123123"+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 	} else {
 		fmt.Fprint(w, "_我不知道你在说什么_")
 	}
