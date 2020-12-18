d4eeeb4d2b9238f6ddd1c029f105d55329ceaeff
LiaoHQ <1915443708@qq.com>
2020-12-18 19:46:46 +0800

    module初始化以及第三方路由宝

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func defaultHander(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是goblog</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "_我不知道你在说什么_")
-	}
+	fmt.Fprint(w, "<h1>Hello, 这里是goblog</h1>")
 }
 
 func aboutHander(w http.ResponseWriter, r *http.Request) {
@@ -21,9 +18,39 @@ func aboutHander(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "_我不知道你在说什么_")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章ID："+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "访问文章列表~~~")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "创建新的文章")
+}
+
 func main() {
-	router := http.NewServeMux()
-	router.HandleFunc("/", defaultHander)
-	router.HandleFunc("/about", aboutHander)
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHander).Methods("GET").Name("about")
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	// router.NotFoundHandler = http.HandleFunc(notFoundHandler)
+
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeUrl:", homeURL)
+	articlesURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articlesUrl:", articlesURL)
+
 	http.ListenAndServe(":3000", router)
 }
